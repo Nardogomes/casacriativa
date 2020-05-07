241ce412bafe6eacabd019461849495c7f475f6c
Ednardo Gomes <ednardo@gomes.com>
2020-05-07 17:53:55 -0300

    Configurando rota em server.js para ideias.html

--- server.js
+++ server.js
@@ -2,12 +2,19 @@
 const express = require('express')
 const server = express()
 
+//Configurar arquivos estáticos.
+server.use(express.static("public"))
+
 /*
 *Criando uma rota
 *Capturando pedido do client para responder
 */
 server.get("/", function(req, res) {
     return res.sendFile(__dirname + "/index.html")
+})
+
+server.get("/ideias", function(req, res) {
+    return res.sendFile(__dirname + "/ideias.html")
 })
 
 //Ligando servidor na porta 3000
