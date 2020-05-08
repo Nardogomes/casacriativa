cd9466aa83df8f56a139dd92ef15ad73a5cdbd3e
Ednardo Gomes <ednardo@email.com>
2020-05-08 19:55:30 -0300

    Alterando index.html para receber conteúdo por variável.

--- server.js
+++ server.js
@@ -2,6 +2,37 @@
 const express = require('express')
 const server = express()
 
+const ideas = [
+    {
+        img: "https://image.flaticon.com/icons/svg/2729/2729007.svg",
+        title: "Curso de Programação",
+        category: "Estudo",
+        description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit neque corrupti distinctio",
+        url: "#"
+    },
+    {
+        img: "https://image.flaticon.com/icons/svg/2729/2729005.svg",
+        title: "Exercícios",
+        category: "Saúde",
+        description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit neque corrupti distinctio",
+        url: "#"
+    },
+    {
+        img: "https://image.flaticon.com/icons/svg/2729/2729027.svg",
+        title: "Curso de Meditação",
+        category: "Saúde da mente",
+        description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit neque corrupti distinctio",
+        url: "#"
+    },
+    {
+        img: "https://image.flaticon.com/icons/svg/2729/2729032.svg",
+        title: "karaokê",
+        category: "Diversão em família",
+        description: "Lorem ipsum dolor, sit amet consectetur adipisicing elit neque corrupti distinctio",
+        url: "#"
+    },
+]
+
 //Configurar arquivos estáticos.(css, scripts, imagens)
 server.use(express.static("public"))
 
@@ -17,7 +48,7 @@ nunjucks.configure("views", {
 *Capturando pedido do client para responder
 */
 server.get("/", function(req, res) {
-    return res.render("index.html")
+    return res.render("index.html", { ideas })
 })
 
 server.get("/ideias", function(req, res) {
